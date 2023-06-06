98e61d987c33bf3f34787aa6fc729ce0cab5e67c
Papika0 <72442767+Papika0@users.noreply.github.com>
2023-06-06 22:58:21 +0400

    feat - add register modal / input field / Gmail and password eye icons

---
+++ src/components/modals/RegisterModal.vue
@@ -0,0 +1,41 @@
+<template>
+    <LayoutModal @close="modalStore.toggleRegisterModal">
+        <template v-slot:header>
+            <h2 class="text-white text-3xl font-bold">Create an account</h2>
+            <h3 class="text-placeholder">Start your journey!</h3>
+        </template>
+
+        <template v-slot:body>
+            <Form class="flex flex-col gap-4 w-full">
+                <InputField name="name" label="Name" placeholder="At least 3 & max.15 lower case characters" />
+                <InputField name="email" label="Email" placeholder="Enter your email" />
+                <InputField name="password" label="Password" placeholder="At least 8 & max.15 lower case characters"
+                    textType="password" />
+                <InputField name="passwordConfirm" label="Confirm Password" placeholder="Confirm your password"
+                    textType="password" />
+                <ButtoneRed text="Get started" />
+                <ButtonDark text="Sign up with Google" :gmail="true" />
+            </Form>
+        </template>
+        <template v-slot:footer>
+            <div class="flex gap-1 mx-auto">
+                <p class="text-placeholder">Already have an account?</p>
+                <span class="underline text-my-blue cursor-pointer" @click="modalStore.switchLoginRegisterModal">Log
+                    in</span>
+            </div>
+        </template>
+    </LayoutModal>
+</template>
+  
+
+<script setup>
+import LayoutModal from '@/components/layouts/LayoutModal.vue';
+import { Form } from 'vee-validate';
+import { useModalStore } from '@/stores/useModalStore.js';
+import InputField from '@/components/ui/InputField.vue';
+import ButtoneRed from '@/components/ui/ButtonRed.vue';
+import ButtonDark from '@/components/ui/ButtonDark.vue';
+
+const modalStore = useModalStore();
+
+</script>
--- src/components/ui/ButtonDark.vue
+++ src/components/ui/ButtonDark.vue
@@ -1,14 +1,22 @@
 <template>
-    <button class="py-2 px-6 text-white rounded border border-white">
-        {{ text }}
+    <button class="py-2 px-6 text-white rounded border border-white flex gap-2 items-center justify-center">
+        <span class=" justify-center text-center items-center" v-if="gmail">
+            <IconGmailLogo />
+        </span> {{ text }}
     </button>
 </template>
 
 <script setup>
+import IconGmailLogo from '../icons/IconGmailLogo.vue';
+
 defineProps({
     text: {
         type: String,
         required: true
+    },
+    gmail: {
+        type: Boolean,
+        default: false
     }
 })
 </script>
