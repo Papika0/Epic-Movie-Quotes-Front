86b4bb1a92fb9fdd4552d3e34091a917995c5b85
Papika0 <72442767+Papika0@users.noreply.github.com>
2023-06-07 23:45:25 +0400

    feat - add vee-validate rules

---
+++ src/plugins/vee-validate/rules.js
@@ -0,0 +1,12 @@
+import { defineRule } from "vee-validate";
+import { required, email, min, max ,alpha_num , confirmed} from "@vee-validate/rules";
+
+defineRule("required", required);
+defineRule("email", email);
+defineRule("min", min);
+defineRule("max", max);
+defineRule("alpha_num", alpha_num);
+defineRule("confirmed", confirmed);
+
+defineRule('lowercase', value => value === value.toLowerCase());
+
