055652036ad2e5c897befe0f8220d68d14092343
Papika0 <72442767+Papika0@users.noreply.github.com>
2023-06-13 12:37:44 +0400

    feat - modals in store, handle password reset, email resend

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -14,6 +14,12 @@
   <LoginModal v-if="modalStore.showLoginModal" />
   <EmailSendModal v-if="modalStore.showEmailSentModal" />
   <EmailVerifiedModal v-if="modalStore.showEmailVerifiedModal" />
+  <EmailForgotPasswordModal v-if="modalStore.showEmailForgotPasswordModal" />
+  <EmailForgotPasswordSendModal v-if="modalStore.showEmailForgotPasswordSendModal" />
+  <ResetPasswordModal v-if="modalStore.showResetPasswordModal" />
+  <EmailVerificationResendModal v-if="modalStore.showEmailVerificationResendModal" />
+  <ResetPasswordResendModal v-if="modalStore.showResetPasswordResendModal" />
+  <ResetPasswordSuccessModal v-if="modalStore.showResetPasswordSuccessModal" />
   <div :class="modalStore.getModalActiveState ? 'h-screen' : ''">
     <section class="flex flex-col items-center mt-80  ">
       <p class="text-6xl text-center text-header font-bold font-title">Find any quote in <br>
@@ -54,6 +60,12 @@ import RegisterModal from '@/components/modals/RegisterModal.vue';
 import LoginModal from '@/components/modals/LoginModal.vue';
 import EmailSendModal from '@/components/modals/EmailSendModal.vue';
 import EmailVerifiedModal from '@/components/modals/EmailVerifiedModal.vue';
+import EmailForgotPasswordModal from '@/components/modals/EmailForgotPasswordModal.vue';
+import EmailForgotPasswordSendModal from '@/components/modals/EmailForgotPasswordSendModal.vue';
+import ResetPasswordModal from '@/components/modals/ResetPasswordModal.vue';
+import EmailVerificationResendModal from '@/components/modals/EmailVerificationResendModal.vue';
+import ResetPasswordResendModal from '@/components/modals/ResetPasswordResendModal.vue';
+import ResetPasswordSuccessModal from '@/components/modals/ResetPasswordSuccessModal.vue';
 import { useModalStore } from '@/stores/useModalStore.js';
 
 
