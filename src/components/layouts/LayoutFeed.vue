1704e11cc21591db58b685369cda4be2ed7ba4b4
Papika0 <72442767+Papika0@users.noreply.github.com>
2023-06-29 00:26:15 +0400

    feat - nav color when movie is route

--- src/components/layouts/LayoutFeed.vue
+++ src/components/layouts/LayoutFeed.vue
@@ -15,9 +15,9 @@
                     <IconHouse class="ml-3" />
                     <p class="text-white text-2xl cursor-pointer">{{ $t('texts.news_feed') }}</p>
                 </div>
-                <div class="flex flex-row gap-10 my-auto">
-                    <IconMovie class="ml-3" />
-                    <p class="text-white text-2xl cursor-pointer">{{ $t('texts.list_of_movies') }}</p>
+                <div class="flex flex-row gap-10 my-auto cursor-pointer" @click="showMovies">
+                    <IconMovie class="ml-3 " :isMovieRoute="isMovieRoute" />
+                    <p class="text-white text-2xl ">{{ $t('texts.list_of_movies') }}</p>
                 </div>
             </div>
         </section>
@@ -34,6 +34,9 @@ import IconHouse from '@/components/icons/profile/IconHouse.vue';
 import IconMovie from '@/components/icons/profile/IconMovie.vue';
 import { useUserStore } from '@/stores/useUserStore';
 import router from '@/router/index.js';
+import { useRoute } from 'vue-router';
+
+const route = useRoute();
 
 const profileImageUrl = computed(() => {
     const thumbnail = useUserStore().user?.thumbnail;
@@ -43,7 +46,15 @@ const profileImageUrl = computed(() => {
     return null;
 });
 
+const showMovies = () => {
+    router.push({ name: 'movies' });
+};
+
 const editProfile = () => {
     router.push({ name: 'profile' });
 };
+
+const isMovieRoute = computed(() => {
+    return route.path.includes('/movies') || route.path.startsWith('/movie/');
+});
 </script>
