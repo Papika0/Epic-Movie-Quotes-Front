90cee7df8d81e9dae2511ac9f369a1aacdbf7df1
Papika0 <72442767+Papika0@users.noreply.github.com>
2023-07-10 00:34:00 +0400

    feat - add quote translation files

--- src/i18n/lang/en/index.js
+++ src/i18n/lang/en/index.js
@@ -2,12 +2,14 @@ import auth from './auth.json'
 import texts from './texts.json'
 import profile from './profile.json'
 import movies from './movies.json'
+import quotes from './quotes.json'
 
 const en = {
     auth,
     texts,
     profile,
-    movies
+    movies, 
+    quotes
 }
 
 export default en;
--- src/i18n/lang/ge/index.js
+++ src/i18n/lang/ge/index.js
@@ -2,12 +2,14 @@ import auth from './auth.json'
 import texts from './texts.json'
 import profile from './profile.json'
 import movies from './movies.json'
+import quotes from './quotes.json'
 
 const ge = {
     auth,
     texts,
     profile,
-    movies
+    movies, 
+    quotes
 }
 
 export default ge;
