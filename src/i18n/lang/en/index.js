ab77196c17416c17e5205a7ea75c48415ab879ca
Papika0 <72442767+Papika0@users.noreply.github.com>
2023-07-10 01:40:30 +0400

    feat - translation files notifications and news feed

--- src/i18n/lang/en/index.js
+++ src/i18n/lang/en/index.js
@@ -3,13 +3,15 @@ import texts from './texts.json'
 import profile from './profile.json'
 import movies from './movies.json'
 import quotes from './quotes.json'
+import feed from './feed.json'
 
 const en = {
     auth,
     texts,
     profile,
     movies, 
-    quotes
+    quotes,
+    feed
 }
 
 export default en;
