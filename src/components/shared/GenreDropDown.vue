f60b80ed2953e7c1c24ce63cedf8a0da4a8c5d4e
Papika0 <72442767+Papika0@users.noreply.github.com>
2023-07-12 00:18:17 +0400

    feat - crud input validation styling and guard fix

--- src/components/shared/GenreDropDown.vue
+++ src/components/shared/GenreDropDown.vue
@@ -1,21 +1,24 @@
 <template>
     <div class="relative ">
         <div class="flex flex-col gap-1">
-            <div class="min-h-[48px] px-4 overflow-x-auto py-2 bg-neutral-900 rounded border border-gray-500  items-center flex flex-wrap max-w-full gap-1"
-                @click="toggleDropdown">
-                <p class="text-gray-500 font-normal leading-loose" v-if="showPlaceholder">Genres</p>
-                <div v-for="option in selected" :key="option.id" class="p-0.5 rounded-sm justify-start items-start flex ">
-                    <div class="rounded-sm items-center flex flex-row justify-between px-1 py-1 bg-gray-500">
-                        <p class="text-white text-sm font-normal leading-snug">{{ options.find((opt) => opt.id ===
-                            option)?.label
-                        }}
-                        </p>
-                        <IconCloseX class="w-4 h-4 relative cursor-pointer" @click.stop="deleteOption(option)" />
+            <Field name="genres" type="hidden" :rules="rules" :value="selected" v-slot="{ meta }">
+                <div class="min-h-[48px] px-4 overflow-x-auto py-2 bg-neutral-900 rounded border border-gray-500  items-center flex flex-wrap max-w-full gap-1"
+                    @click="toggleDropdown"
+                    :class="[meta.touched && !meta.valid ? 'border-red-500' : (meta.touched && meta.valid ? 'border-green-500' : '')]">
+                    <p class="text-gray-500 font-normal leading-loose" v-if="showPlaceholder">Genres</p>
+                    <div v-for="option in selected" :key="option.id"
+                        class="p-0.5 rounded-sm justify-start items-start flex ">
+                        <div class="rounded-sm items-center flex flex-row justify-between px-1 py-1 bg-gray-500">
+                            <p class="text-white text-sm font-normal leading-snug">{{ options.find((opt) => opt.id ===
+                                option)?.label
+                            }}
+                            </p>
+                            <IconCloseX class="w-4 h-4 relative cursor-pointer" @click.stop="deleteOption(option)" />
+                        </div>
                     </div>
                 </div>
-            </div>
-            <ErrorMessage name="genres" class="pl-2 pt-1 text-red-star text-sm" />
-            <Field name="genres" type="hidden" :rules="rules" :value="selected" />
+                <ErrorMessage name="genres" class="pl-2 pt-1 text-red-star text-sm" />
+            </Field>
         </div>
         <ul v-if="isDropdownOpen"
             class="absolute mt-3 left-0 w-full bg-neutral-900 py-2 rounded border border-gray-500 shadow-md z-50 max-h-56 overflow-y-scroll">
